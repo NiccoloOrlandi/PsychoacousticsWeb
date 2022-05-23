fc4b2e399de82e74b9c2fd0667160fbaa95cf6f2
AndreaFel <and.felline@gmail.com>
2022-05-23 17:53:18 +0200

    ri-rimosso soundSettingsValidation.js

--- soundSettings.php
+++ soundSettings.php
@@ -8,7 +8,6 @@
 		<!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 		<link rel="stylesheet" href="staircaseStyle.css">
-		<script type="text/javascript" src="soundSettingsValidation.js" defer></script>
 		
 		<?php 
 			session_start(); 
